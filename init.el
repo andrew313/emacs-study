ad2243b0152d973ffff28a3f53fd70df90aa882d
andrew <aspen1yang313@gmail.com>
2018-01-03 09:48:17 +0800

    add init.el .spacemacs

---
+++ init.el
@@ -0,0 +1,36 @@
+;;; init.el --- Spacemacs Initialization File
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
+
+;; Increase gc-cons-threshold, depending on your system you may set it back to a
+;; lower value in your dotfile (function `dotspacemacs/user-config')
+(setq gc-cons-threshold 100000000)
+
+(load-file (concat (file-name-directory load-file-name)
+                   "core/core-versions.el"))
+(load-file (concat (file-name-directory load-file-name)
+                   "core/core-load-paths.el"))
+
+(if (not (version<= spacemacs-emacs-min-version emacs-version))
+    (error (concat "Your version of Emacs (%s) is too old. "
+                   "Spacemacs requires Emacs version %s or above.")
+           emacs-version spacemacs-emacs-min-version)
+  (require 'core-spacemacs)
+  (configuration-layer/load-lock-file)
+  (spacemacs/init)
+  (configuration-layer/stable-elpa-download-tarball)
+  (configuration-layer/load)
+  (spacemacs-buffer/display-startup-note)
+  (spacemacs/setup-startup-hook)
+  (require 'server)
+  (unless (server-running-p) (server-start)))
